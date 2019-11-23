a00ebdf92598f42a298f3be3af663af2b19f0459
Steve Warsa <swarsa480@gmail.com>
2019-11-23 12:45:20 -0700

    First 5 exercises from the Coding interview bootcamp on algorithms (Udemy course)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,22 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
+  let spy = null;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
+    spy = spyOn(console, "log");
   }));
 
-  it('should create the app', () => {
+  afterEach(() => {
+    if (spy) {
+      spy.calls.reset();
+    }
+  });
+
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
@@ -19,13 +25,107 @@ describe('AppComponent', () => {
   it(`should have as title 'test-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('test-project');
+    expect(app.title).toEqual("test-project");
   });
 
-  it('should render title in a h1 tag', () => {
+  it("should render title in a h1 tag", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to test-project!');
+    expect(compiled.querySelector("h1").textContent).toContain(
+      "Welcome to test-project!"
+    );
+  });
+
+  it("Should return correct string for #15", () => {
+    expect(AppComponent.fizzBizz(15)).toEqual("FizzBizz");
+  });
+  it("Should return correct string for #9", () => {
+    expect(AppComponent.fizzBizz(9)).toEqual("fizz");
+  });
+  it("Should return correct string for #25", () => {
+    expect(AppComponent.fizzBizz(25)).toEqual("bizz");
+  });
+  it("Should return correct string for #7", () => {
+    expect(AppComponent.fizzBizz(7)).toEqual("7");
+  });
+  it("Should return 2 chunks [[1,2],[3,4]]", () => {
+    expect(AppComponent.chunk([1, 2, 3, 4], 2)).toEqual([
+      [1, 2],
+      [3, 4]
+    ]);
+  });
+  it("Should return true for 'rail safety', 'fairy tales'", () => {
+    expect(AppComponent.anagrams("rail safety", "fairy tales")).toBeTruthy();
+  });
+  it("Should return true for 'RAIL! SAFETY!', 'fairy tales'", () => {
+    expect(AppComponent.anagrams("RAIL! SAFETY!", "fairy tales")).toBeTruthy();
+  });
+  it("Should return false for 'Hi there', 'Bye there'", () => {
+    expect(AppComponent.anagrams("Hi there", "Bye there")).toBeFalsy();
+  });
+  it("anagrams function exists", () => {
+    expect(typeof AppComponent.anagrams).toEqual("function");
+  });
+
+  it('"hello" is an anagram of "llohe"', () => {
+    expect(AppComponent.anagrams("hello", "llohe")).toBeTruthy();
+  });
+
+  it('"Whoa! Hi!" is an anagram of "Hi! Whoa!"', () => {
+    expect(AppComponent.anagrams("Whoa! Hi!", "Hi! Whoa!")).toBeTruthy();
+  });
+
+  it('"One One" is not an anagram of "Two two two"', () => {
+    expect(AppComponent.anagrams("One One", "Two two two")).toBeFalsy();
+  });
+
+  it('"One one" is not an anagram of "One one c"', () => {
+    expect(AppComponent.anagrams("One one", "One one c")).toBeFalsy();
+  });
+
+  it('"A tree, a life, a bench" is not an anagram of "A tree, a fence, a yard"', () => {
+    expect(
+      AppComponent.anagrams(
+        "A tree, a life, a bench",
+        "A tree, a fence, a yard"
+      )
+    ).toBeFalsy();
+  });
+  it("Capitalize is a function", () => {
+    expect(typeof AppComponent.capitalize).toEqual("function");
+  });
+
+  it("capitalizes the first letter of every word in a sentence", () => {
+    expect(AppComponent.capitalize("hi there, how is it going?")).toEqual(
+      "Hi There, How Is It Going?"
+    );
+  });
+
+  it("capitalizes the first letter", () => {
+    expect(
+      AppComponent.capitalize("i love breakfast at bill miller bbq")
+    ).toEqual("I Love Breakfast At Bill Miller Bbq");
+  });
+  it("steps is a function", () => {
+    expect(typeof AppComponent.steps).toEqual("function");
+  });
+
+  it("steps called with n = 1", () => {
+    AppComponent.steps(1);
+    expect(console.log).toHaveBeenCalledWith("#");
+  });
+
+  it("steps called with n = 2", () => {
+    AppComponent.steps(2);
+    expect(console.log).toHaveBeenCalledWith("# ");
+    expect(console.log).toHaveBeenCalledWith("##");
+  });
+
+  it("steps called with n = 3", () => {
+    AppComponent.steps(3);
+    expect(console.log).toHaveBeenCalledWith("#  ");
+    expect(console.log).toHaveBeenCalledWith("## ");
+    expect(console.log).toHaveBeenCalledWith("###");
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,105 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'test-project';
+  title = "test-project";
+
+  public static fizzBizz(num: number): string {
+    if (num % 15 === 0) {
+      return "FizzBizz";
+    } else if (num % 3 === 0) {
+      return "fizz";
+    } else if (num % 5 === 0) {
+      return "bizz";
+    } else {
+      return num.toString();
+    }
+  }
+
+  public static chunk(array: any[], size: number): any[] {
+    const chunked = [];
+    let index = 0;
+    while (index < array.length) {
+      chunked.push(array.slice(index, index + size));
+      index += size;
+    }
+    return chunked;
+  }
+  public static anagrams(stringA: string, stringB: string): boolean {
+    return this.cleanup(stringA) === this.cleanup(stringB);
+  }
+  private static cleanup(str: string): string {
+    return str
+      .replace(/[^\w]/g, "")
+      .toLowerCase()
+      .split("")
+      .sort()
+      .join("");
+  }
+  // public static anagrams(stringA: string, stringB: string): boolean {
+  //   let stringAChrs = AppComponent.createCharMap(stringA);
+  //   let stringBChrs = AppComponent.createCharMap(stringB);
+  //   if (Object.keys(stringBChrs).length !== Object.keys(stringAChrs).length) {
+  //     return false;
+  //   }
+  //   for (let chr in stringAChrs) {
+  //     if (!stringBChrs[chr] || stringAChrs[chr] !== stringBChrs[chr]) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // }
+  // private static createCharMap(str: string): any {
+  //   let charMap = {};
+  //   str
+  //     .toLowerCase()
+  //     .replace(/[^\w]/g, "") // replace every character that is not a letter or number
+  //     .split("")
+  //     .forEach(chr => (charMap[chr] ? charMap[chr]++ : (charMap[chr] = 1)));
+  //   return charMap;
+  // }
+
+  public static capitalize(str: string): string {
+    let newSentence = "";
+    for (let word of str.split(" ")) {
+      newSentence += word.charAt(0).toUpperCase() + word.substr(1) + " ";
+    }
+    return newSentence.trim();
+  }
+
+  // alternative 'recursive' solution (basically copied from instructor)
+  public static steps(num: number, row: number = 0, stair: string = "") {
+    if (num === row) {
+      return;
+    }
+
+    if (num === stair.length) {
+      // this is the final recursive invocation for this row, so print the row
+      console.log(stair);
+      // reset the stair for the next row and then make the recursive call
+      this.steps(num, row + 1, "");
+      return;
+    }
+
+    // decide what character to append
+    stair += stair.length <= row ? "#" : " ";
+
+    // make the recursive call again with the new values - note: we're still building the
+    // same console log message (row), so don't increment the row
+    this.steps(num, row, stair);
+  }
+  // first solution (my first shot - no help)
+  // public static steps(num: number) {
+  //     for (let i = 0; i < num; i++) {
+  //     let arr: string[] = [];
+  //     for (let j = 0; j < num; j++) {
+  //       arr.push(j <= i ? "#" : " ");
+  //     }
+  //     console.log(arr.join(""));
+  //   }
+  // }
 }
